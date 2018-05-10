7e2b05d79dae9320ac283a134901a96e44d1cf24
Lucas <lmckamey@student.neumont.edu>
2018-05-10 11:14:48 -0600

    Audio and Some Scripts
    Added some Sound effects that I thougth would be fun,  Also added my Scritps, (Game and End Manager)

---
+++ Assets/Scripts/EndManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndManager : Singleton<EndManager>
+{
+    [SerializeField] GameObject m_endPanel;
+
+    public void GameOver()
+    {
+        m_endPanel.SetActive(true);
+    }
+}
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,25 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Game : MonoBehaviour
+public class Game : Singleton<Game>
 {
 
-    public Difficulty difficulty { get; set; }
-    public GameMode gameMode { get; set; }
+    public Difficulty Difficulty { get; set; }
+    public GameMode GameMode { get; set; }
+    static Game ms_instance = null;
     void Start()
     {
-
+        DontDestroyOnLoad(this.gameObject);
+        if (ms_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            ms_instance = this;
+        }
     }
-    
 
     void LoadScene(string scene)
-    {   
+    {
         SceneManager.LoadScene(scene);
     }
 
     void QuitGame()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 
 }
