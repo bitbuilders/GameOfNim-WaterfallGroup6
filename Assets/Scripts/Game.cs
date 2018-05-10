5889cd7ac8dfbb5daf92ba75cba3cd31fce23578
Lucas <lmckamey@student.neumont.edu>
2018-05-10 10:51:33 -0600

    Added GameManager(renamed to Game)
    Same

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+
+    public Difficulty difficulty { get; set; }
+    public GameMode gameMode { get; set; }
+    void Start()
+    {
+
+    }
+    
+
+    void LoadScene(string scene)
+    {   
+        SceneManager.LoadScene(scene);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
