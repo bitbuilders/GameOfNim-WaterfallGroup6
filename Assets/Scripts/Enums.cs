3fd5d4c97b610bf6914d3c8867837cc3c5655122
Colin Misbach <colin@misbach.com>
2018-05-10 10:39:31 -0600

    Added enums folder

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Difficulty
+{
+    EASY,
+    MEDIUM,
+    HARD
+}
+
+public enum GameMode
+{
+    PvP,
+    PvC
+}
+
+public enum PlayerType
+{
+    PLAYER_1,
+    PLAYER_2
+}
